f3e6db3ad5f75768b791ae412d4f344b60248ec2
Tariq Cranston <32856620+TariqC7@users.noreply.github.com>
2019-02-22 22:41:56 -0500

    The main function
    This is the basic predicate function that returns true if N is the sum of the numbers not in nested lists in L and zero if there are no such numbers or elements in the list.

--- prolog.pl
+++ prolog.pl
@@ -9,3 +9,9 @@ sum-up-numbers-simple([[100]],0).
 sum-up-numbers-simple([100,[200]],100).
 sum-up-numbers-simple([a,100,b,[200],c,300,d],400).
 sum-up-numbers-simple([a,100,b,200,c,300,d],600).
+
+sum-up-numbers-simple([L | N],TotalSum) :-
+	sum-up-numbers-simple(N,Sum1),
+	TotalSum is L + Sum1.
+
+% #2-sum-up-numbers-general 
