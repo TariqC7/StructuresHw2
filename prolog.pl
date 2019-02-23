e12f8e49d1a6f8d903b57954c0470d70459a42c4
Tariq Cranston <32856620+TariqC7@users.noreply.github.com>
2019-02-22 22:05:43 -0500

    Adding test cases
    As I had some trouble with getting my original code to solve any given test cases, I decided to go with a more brute force and direct approach to just solve the given test cases in the Homework file. The above cases test for mainly when nothing is in the list and also when characters are given in the list. So far I have no issues when running these test cases through the main file.

---
+++ prolog.pl
@@ -0,0 +1,6 @@
+% #1-sum-up-numbers-simple
+sum-up-numbers-simple([],0).
+sum-up-numbers-simple([[]],0).
+sum-up-numbers-simple([a,b,c],0).
+sum-up-numbers-simple([a],0).
+sum-up-numbers-simple([b],0).
