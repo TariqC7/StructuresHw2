7049cad0ee9d51b4ac2964e29021880266cd776d
Tariq Cranston <32856620+TariqC7@users.noreply.github.com>
2019-02-22 22:23:43 -0500

    Adding Test Cases
    These are test cases mainly for lists with numbers and characters. After testing all of these; they all returned true and gave the value that they were supposed to.

--- prolog.pl
+++ prolog.pl
@@ -4,3 +4,8 @@ sum-up-numbers-simple([[]],0).
 sum-up-numbers-simple([a,b,c],0).
 sum-up-numbers-simple([a],0).
 sum-up-numbers-simple([b],0).
+sum-up-numbers-simple([c],0).
+sum-up-numbers-simple([[100]],0).
+sum-up-numbers-simple([100,[200]],100).
+sum-up-numbers-simple([a,100,b,[200],c,300,d],400).
+sum-up-numbers-simple([a,100,b,200,c,300,d],600).
